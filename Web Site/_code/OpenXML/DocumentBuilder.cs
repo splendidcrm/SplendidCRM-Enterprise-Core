96f68ac4fb8f0f74cc0d994e2fe932b1c1f8aa25
Paul Rony <paul@splendidcrm.com>
2025-08-29 14:19:05 -0400

    Fix issue building for github

--- Web Site/_code/OpenXML/DocumentBuilder.cs
+++ Web Site/_code/OpenXML/DocumentBuilder.cs
@@ -1,23 +1,72 @@
-/**********************************************************************************************************************
- * SplendidCRM is a Customer Relationship Management program created by SplendidCRM Software, Inc. 
- * Copyright (C) 2005-2023 SplendidCRM Software, Inc. All rights reserved.
- *
- * Any use of the contents of this file are subject to the SplendidCRM Enterprise Source Code License 
- * Agreement, or other written agreement between you and SplendidCRM ("License"). By installing or 
- * using this file, you have unconditionally agreed to the terms and conditions of the License, 
- * including but not limited to restrictions on the number of users therein, and you may not use this 
- * file except in compliance with the License. 
- * 
- * SplendidCRM owns all proprietary rights, including all copyrights, patents, trade secrets, and 
- * trademarks, in and to the contents of this file.  You will not link to or in any way combine the 
- * contents of this file or any derivatives with any Open Source Code in any manner that would require 
- * the contents of this file to be made available to any third party. 
- * 
- * IN NO EVENT SHALL SPLENDIDCRM BE RESPONSIBLE FOR ANY DAMAGES OF ANY KIND, INCLUDING ANY DIRECT, 
- * SPECIAL, PUNITIVE, INDIRECT, INCIDENTAL OR CONSEQUENTIAL DAMAGES.  Other limitations of liability 
- * and disclaimers set forth in the License. 
- * 
- *********************************************************************************************************************/
+/***************************************************************************
+
+Copyright (c) Microsoft Corporation 2009.
+
+This code is licensed using the Microsoft Public License (Ms-PL).  The text of the license can be found here:
+
+http://www.microsoft.com/resources/sharedsource/licensingbasics/publiclicense.mspx
+
+***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OpenXml.PowerTools
+{
+    /// <summary>
+    /// Source is a simple helper class used for arguments to the document building functions	
+    /// </summary>
+    public class Source
+    {
+        private static XNamespace ns = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
+
+        private WordprocessingDocument m_Source;
+        private IEnumerable<XElement> m_Contents;
+        private bool m_KeepSections;
+
+        /// <summary>
+        /// Specify the entire source document	
+        /// </summary>
+        public Source(WordprocessingDocument source, bool keepSections)
+        {
+            m_Source = source;
+            m_Contents = source.MainDocumentPart.GetXDocument().Descendants(ns + "body").Elements();
+            m_KeepSections = keepSections;
+        }
+        /// <summary>
+        /// Specify from "start" to the end of the document	
+        /// </summary>
+        public Source(WordprocessingDocument source, int start, bool keepSections)
+        {
+            m_Source = source;
+            m_Contents = source.MainDocumentPart.GetXDocument().Descendants(ns + "body").Elements().Skip(start);
+            m_KeepSections = keepSections;
+        }
+        /// <summary>
+        /// Specify from "start" and include "count" number of paragraphs
+        /// </summary>
+        public Source(WordprocessingDocument source, int start, int count, bool keepSections)
+        {
+            m_Source = source;
+            m_Contents = source.MainDocumentPart.GetXDocument().Descendants(ns + "body").Elements().Skip(start).Take(count);
+            m_KeepSections = keepSections;
+        }
+        /// <summary>
+        /// Close the source document	
+        /// </summary>
+        /*
+        public void Close()
+        {
+            m_Source.Close();
+        }
+        */
 
         internal WordprocessingDocument Document
         {
